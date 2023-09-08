9f924e52df2d1c8f78a2e0237d1aeea08843fe5a
correakarl <karl.correa.88@gmail.com>
2023-09-07 21:25:16 -0400

    asdf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from 'src/components/header/header.component';
-import { MainComponent } from 'src/components/main/main.component';
-import { FooterComponent } from 'src/components/footer/footer.component';
+import { HeaderComponent } from 'src/components/layouts/header/header.component';
+import { MainComponent } from 'src/components/layouts/main/main.component';
+import { FooterComponent } from 'src/components/layouts/footer/footer.component';
 import { MatCardModule} from "@angular/material/card"
 import { MatButtonModule } from "@angular/material/button"
 import { MatIconModule } from '@angular/material/icon'
